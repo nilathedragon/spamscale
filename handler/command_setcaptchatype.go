037cf29ad0a44728c518793079727b313ff921c0
NilaTheDragon <nilathedragon@pm.me>
2025-11-09 23:45:18 +0100

    cleanup setcaptchatype command

--- handler/command_setcaptchatype.go
+++ handler/command_setcaptchatype.go
@@ -22,33 +22,19 @@ func CommandSetCaptchaTypeHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	}
 
 	if !util.IsAdmin(b, ctx.EffectiveChat.Id, ctx.EffectiveUser.Id) {
-		_, err := b.SendMessage(ctx.EffectiveChat.Id, "You are not authorized to use this command", &gotgbot.SendMessageOpts{})
-		if err != nil {
-			return err
-		}
-		return nil
+		return util.TempMessage(b, ctx.EffectiveChat.Id, "You are not authorized to use this command")
 	}
 
-	keyboard := util.GenerateKeyboard(model.CaptchaTypes, CommandSetCaptchaTypeCallback, 2)
-	_, err := b.SendMessage(ctx.EffectiveChat.Id, "Please select the type of captcha you want to use", &gotgbot.SendMessageOpts{
+	return util.DropMessage(b.SendMessage(ctx.EffectiveChat.Id, "Please select the type of captcha you want to use", &gotgbot.SendMessageOpts{
 		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
-			InlineKeyboard: keyboard,
+			InlineKeyboard: util.GenerateKeyboard(model.CaptchaTypes, CommandSetCaptchaTypeCallback, 2),
 		},
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
+	}))
 }
 
 func CommandSetCaptchaTypeHandlerCallback(b *gotgbot.Bot, ctx *ext.Context) error {
 	if !util.IsAdmin(b, ctx.EffectiveChat.Id, ctx.EffectiveUser.Id) {
-		_, err := b.SendMessage(ctx.EffectiveChat.Id, "You are not authorized to use this command", &gotgbot.SendMessageOpts{})
-		if err != nil {
-			return err
-		}
-		return nil
+		return util.TempMessage(b, ctx.EffectiveChat.Id, "You are not authorized to use this command")
 	}
 
 	if _, err := b.DeleteMessage(ctx.EffectiveChat.Id, ctx.EffectiveMessage.MessageId, &gotgbot.DeleteMessageOpts{}); err != nil {
@@ -57,19 +43,12 @@ func CommandSetCaptchaTypeHandlerCallback(b *gotgbot.Bot, ctx *ext.Context) erro
 
 	captchaType := ctx.CallbackQuery.Data[len(CommandSetCaptchaTypeCallback):]
 	if !slices.Contains(model.CaptchaTypes, model.CaptchaType(captchaType)) {
-		_, err := b.SendMessage(ctx.EffectiveChat.Id, "Invalid captcha type. Please try again.", &gotgbot.SendMessageOpts{})
-		if err != nil {
-			return err
-		}
-		return nil
+		return util.TempMessage(b, ctx.EffectiveChat.Id, "Invalid captcha type. Please try again.")
 	}
 
 	if err := db.Chat.SetCaptchaType(ctx.EffectiveChat.Id, model.CaptchaType(captchaType)); err != nil {
 		return err
 	}
 
-	if _, err := b.SendMessage(ctx.EffectiveChat.Id, "Captcha type updated successfully", &gotgbot.SendMessageOpts{}); err != nil {
-		return err
-	}
-	return nil
+	return util.TempMessage(b, ctx.EffectiveChat.Id, "Captcha type updated successfully")
 }
