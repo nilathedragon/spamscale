ddb4573acad3e2df0840a096d749ae56783f4a37
drafolin <derg@drafolin.ch>
2025-11-16 00:58:22 +0100

    Update `flake.nix` to disable "fortify" hardening in devShell configuration
    - Added support for go debugger

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: 
-        let
-            pkgs = import nixpkgs { inherit system; };
-        in 
-        {
-            devShells.default = pkgs.mkShell {
-                packages = with pkgs; [
-                    go
-                ];
-            };
-        }
-    );
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+        ];
+
+        hardeningDisable = [ "fortify" ];
+      };
+    }
+  );
 }
